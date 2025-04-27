aef0c362dfd0697e78d897103e7867a591e6a047
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2025-04-27 23:14:52 +0200

    Committing ui todolist UI improvements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,27 +5,6 @@ function App() {
  
 
   return (
-    // <div className="p-[2rem]">
-    //   <h1 className="text-2xl font-bold">Task</h1>
-      
-    //   <div className='flex flex-col gap-2'>
-    //   <input type="text" className="border-2 border-amber-200" data-name="title" value={newTask.title} onChange={handleChange} placeholder="Title" />
-    //   <textarea className="border-2 border-amber-200" data-name="description" value={newTask.description} onChange={handleChange} placeholder="Enter Todo Content" />
-    //   </div>
-     
-    //   <button className="w-[2.5rem] h-[2.5rem] border-2 border-amber-950 m-[0.5rem]" onClick={handleCreateTask}>Add</button> 
-    //   <button className="w-[2.5rem] h-[2.5rem] border-2 border-amber-950 m-[0.5rem]" onClick={handleGetTasks}>Get</button>
-
-    //   <ul>
-    //     {tasks.map((task) => (
-    //       <li key={task.id} className="border-b-2 border-gray-300">
-    //         <h2 className="text-xl font-bold">{task.title}</h2>
-    //         <p>{task.description}</p>
-    //       </li>
-    //     ))}
-    //   </ul>
-    // </div>
-
     <TodoList />
   )
 }
